93af36556296c8a8ad68c6011f1f0cd0533e19db
Pawhncho <pawhncho@gmail.com>
2024-12-22 02:00:10 -0800

    Initial Commit - 2 | Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ReactGA.initialize('G-82C9GX91MD');
+ReactGA.send({ hitType: "pageview", page: "/" });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
